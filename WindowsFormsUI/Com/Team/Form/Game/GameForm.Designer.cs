83b516c3a9f102f1a43005f90343c6553fe68af8
Tal Jacob <taljacob2@gmail.com>
2021-08-27 18:20:20 +0300

    after labelPlayer1 and labelPlayer2

--- WindowsFormsUI/Com/Team/Form/Game/GameForm.Designer.cs
+++ WindowsFormsUI/Com/Team/Form/Game/GameForm.Designer.cs
@@ -34,18 +34,23 @@ namespace WindowsFormsUI.Com.Team.Form.Game
         private void InitializeComponent()
         {
             this.buttonForfeit = new System.Windows.Forms.Button();
+            this.labelPlayer1 = new System.Windows.Forms.Label();
+            this.labelPlayer2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
 
             // 
             // buttonForfeit
             // 
+            const int k_ButtonForfeitWidth = 128;
+            const int k_ButtonForfeitHeight = 44;
             this.buttonForfeit.BackColor = System.Drawing.SystemColors.Highlight;
             this.buttonForfeit.Name = "buttonForfeit";
-            this.buttonForfeit.Size = new System.Drawing.Size(128, 44);
+            this.buttonForfeit.Size = new System.Drawing.Size(k_ButtonForfeitWidth, 44);
             this.buttonForfeit.TabIndex = 0;
             this.buttonForfeit.Text = "Forfeit";
             this.buttonForfeit.UseVisualStyleBackColor = false;
             this.buttonForfeit.Click += new EventHandler(buttonForfeit_Click);
+            
             
             // 
             // buttonColumns
@@ -58,9 +63,40 @@ namespace WindowsFormsUI.Com.Team.Form.Game
             createButtonCoins();
             
             // set Location of `buttonForfeit`:
+            int buttonForfeitHeightLocation = m_MaxButtonCoinHeight
+                                      + this.buttonForfeit.Size.Height * 2;
+            int buttonForfeitWidthLocation = m_CenterWidth;
             this.buttonForfeit.Location = 
-                new System.Drawing.Point(m_CenterWidth, m_MaxButtonCoinHeight
-                 + this.buttonForfeit.Size.Height * 2);
+                new System.Drawing.Point(buttonForfeitWidthLocation,
+                    buttonForfeitHeightLocation);
+            
+            // 
+            // labelPlayer1
+            // 
+            const int k_LabelPlayer1Width = 80; 
+            const int k_LabelPlayer1Height = 29;
+            this.labelPlayer1.Location = new System.Drawing
+                .Point(buttonForfeitWidthLocation - k_LabelPlayer1Width,
+                    buttonForfeitHeightLocation);
+            this.labelPlayer1.Name = "labelPlayer1";
+            this.labelPlayer1.Size = new System.Drawing.Size(k_LabelPlayer1Width,
+                k_LabelPlayer1Height);
+            this.labelPlayer1.TabIndex = 2;
+            this.labelPlayer1.Text = "Player 1:";
+            this.labelPlayer1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            
+            // 
+            // labelPlayer2
+            // 
+            this.labelPlayer2.Location = new System.Drawing
+                .Point(buttonForfeitWidthLocation + k_ButtonForfeitWidth,
+                    buttonForfeitHeightLocation);
+            this.labelPlayer2.Name = "labelPlayer2";
+            this.labelPlayer2.Size = new System.Drawing.Size(k_LabelPlayer1Width,
+                k_LabelPlayer1Height);
+            this.labelPlayer2.TabIndex = 3;
+            this.labelPlayer2.Text = "Player 2:";
+            this.labelPlayer2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 
             // 
             // GameForm
@@ -71,6 +107,8 @@ namespace WindowsFormsUI.Com.Team.Form.Game
             addButtonColumns();
             addButtonCoins();
             this.Controls.Add(this.buttonForfeit);
+            this.Controls.Add(this.labelPlayer1);
+            this.Controls.Add(this.labelPlayer2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.AutoSize = true;
             this.Padding = new Padding(k_Padding);
@@ -96,6 +134,9 @@ namespace WindowsFormsUI.Com.Team.Form.Game
         private Button[] buttonColumns; 
 
         private Button[,] buttonCoins;
+        
+        private System.Windows.Forms.Label labelPlayer1;
+        private System.Windows.Forms.Label labelPlayer2;
 
         #endregion
     }
